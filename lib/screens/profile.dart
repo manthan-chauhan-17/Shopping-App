c3d6c379c3819ccf676eb45fce9873a7d4418f48
manthan-chauhan-17 <manthanchauhan062@gmail.com>
2024-08-17 10:53:20 +0530

    Added User Profile screen

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,4 +1,13 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:random_string/random_string.dart';
+import 'package:shopping_app/screens/onboarding.dart';
+import 'package:shopping_app/services/auth.dart';
+import 'package:shopping_app/services/shared_pref.dart';
+import 'package:shopping_app/widgets/app_widgets.dart';
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -8,8 +17,280 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  String? name, image, email;
+  final ImagePicker _picker = ImagePicker();
+  File? selectedImage;
+
+  getSharedPref() async {
+    image = await SharedPreferenceHelper().getUserImage();
+    name = await SharedPreferenceHelper().getUserName();
+    email = await SharedPreferenceHelper().getUserEmail();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    getSharedPref();
+    super.initState();
+  }
+
+  Future getImage() async {
+    var image = await _picker.pickImage(source: ImageSource.gallery);
+    selectedImage = File(image!.path);
+    uploadItem();
+    setState(() {});
+  }
+
+  uploadItem() async {
+    if (selectedImage != null) {
+      String addId = randomAlphaNumeric(10);
+      Reference firebaseStorageRef =
+          FirebaseStorage.instance.ref().child("blogImage").child(addId);
+
+      final UploadTask task = firebaseStorageRef.putFile(selectedImage!);
+      var downloadUrl = await (await task).ref.getDownloadURL();
+      await SharedPreferenceHelper().saveUserImage(downloadUrl);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xfff2f2f2),
+        title: Text(
+          "Profile",
+          style: AppWidgets.boldWeightTextStyle(),
+        ),
+      ),
+      backgroundColor: const Color(0xfff2f2f2),
+      body: name == null
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Container(
+              child: Column(
+                children: [
+                  // Profile Image
+                  selectedImage != null
+                      ? GestureDetector(
+                          onTap: () {
+                            getImage();
+                          },
+                          child: Center(
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(60),
+                              child: Image.file(
+                                selectedImage!,
+                                height: 150,
+                                width: 150,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        )
+                      : GestureDetector(
+                          onTap: () {
+                            getImage();
+                          },
+                          child: Center(
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(60),
+                              child: Image.asset(
+                                "assets/images/demo-user.jpg",
+                                height: 150,
+                                width: 150,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  // Name
+                  Container(
+                    margin: const EdgeInsets.only(left: 20.0, right: 20.0),
+                    child: Material(
+                      elevation: 3.0,
+                      borderRadius: BorderRadius.circular(10),
+                      child: Container(
+                        padding: const EdgeInsets.all(10.0),
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Row(
+                          children: [
+                            const Icon(
+                              Icons.person_outline,
+                              size: 35,
+                            ),
+                            const SizedBox(
+                              width: 10.0,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Name",
+                                  style: AppWidgets.lightWeightTextStyle(),
+                                ),
+                                Text(
+                                  name!,
+                                  style: AppWidgets.semiBoldTextStyle(),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  // Email
+                  Container(
+                    margin: const EdgeInsets.only(left: 20.0, right: 20.0),
+                    child: Material(
+                      elevation: 3.0,
+                      borderRadius: BorderRadius.circular(10),
+                      child: Container(
+                        padding: const EdgeInsets.all(10.0),
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Row(
+                          children: [
+                            const Icon(
+                              Icons.mail_outline,
+                              size: 35,
+                            ),
+                            const SizedBox(
+                              width: 10.0,
+                            ),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Email",
+                                    style: AppWidgets.lightWeightTextStyle(),
+                                  ),
+                                  Text(
+                                    email!,
+                                    style: AppWidgets.semiBoldTextStyle(),
+                                    overflow: TextOverflow.ellipsis,
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  // Logout
+                  GestureDetector(
+                    onTap: () async {
+                      await AuthMethods().logOut().then(
+                            (value) => Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const Onboarding()),
+                            ),
+                          );
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.only(left: 20.0, right: 20.0),
+                      child: Material(
+                        elevation: 3.0,
+                        borderRadius: BorderRadius.circular(10),
+                        child: Container(
+                          padding: const EdgeInsets.all(10.0),
+                          width: MediaQuery.of(context).size.width,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Row(
+                            children: [
+                              const Icon(
+                                Icons.logout,
+                                size: 35,
+                              ),
+                              const SizedBox(
+                                width: 10.0,
+                              ),
+                              Text(
+                                "Logout",
+                                style: AppWidgets.semiBoldTextStyle(),
+                              ),
+                              const Spacer(),
+                              const Icon(Icons.arrow_forward_outlined)
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  // Delete
+                  GestureDetector(
+                    onTap: () async {
+                      await AuthMethods().deleteUser().then(
+                            (value) => Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const Onboarding()),
+                            ),
+                          );
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.only(left: 20.0, right: 20.0),
+                      child: Material(
+                        elevation: 3.0,
+                        borderRadius: BorderRadius.circular(10),
+                        child: Container(
+                          padding: const EdgeInsets.all(10.0),
+                          width: MediaQuery.of(context).size.width,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Row(
+                            children: [
+                              const Icon(
+                                Icons.delete_outline,
+                                size: 35,
+                              ),
+                              const SizedBox(
+                                width: 10.0,
+                              ),
+                              Text(
+                                "Delete Account",
+                                style: AppWidgets.semiBoldTextStyle(),
+                              ),
+                              const Spacer(),
+                              const Icon(Icons.arrow_forward_outlined)
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+    );
   }
 }
